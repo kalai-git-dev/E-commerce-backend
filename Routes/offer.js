49446aa359afa659c678065da7c64594f867887e
kalai-git-dev <kalai.amine@hotmail.fr>
2021-03-15 20:59:35 +0100

    update publish route

--- Routes/offer.js
+++ Routes/offer.js
@@ -13,43 +13,47 @@ const isAuthenticated = require("../MiIddelwares/isAuthenticated");
 const Offer = require("../Models/Offer");
 
 router.post("/publish", isAuthenticated, async (req, res) => {
-  const {
-    title,
-    description,
-    category,
-    price,
-    sexe,
-    city,
-    marque,
-    size,
-    condition,
-    color,
-  } = req.fields;
-  //   console.log(req.user);
-  console.log(req.files.picture.path);
+  try {
+    const {
+      title,
+      description,
+      category,
+      price,
+      sexe,
+      city,
+      marque,
+      size,
+      condition,
+      color,
+    } = req.fields;
+    //   console.log(req.user);
+    console.log(req.fields);
+    console.log(req.files.picture.path);
+    const newOffer = new Offer({
+      title,
+      description,
+      category,
+      city,
+      price,
+      details: [
+        { SEXE: sexe },
+        { MARQUE: marque },
+        { TAILLE: size },
+        { ETAT: condition },
+        { COULEUR: color },
+      ],
+      owner: req.user,
+    });
+    const result = await cloudinary.uploader.upload(req.files.picture.path, {
+      folder: "/ecomerce/offers",
+    });
+    newOffer.image = result;
+    await newOffer.save();
 
-  const newOffer = new Offer({
-    title,
-    description,
-    category,
-    city,
-    price,
-    details: [
-      { SEXE: sexe },
-      { MARQUE: marque },
-      { TAILLE: size },
-      { ETAT: condition },
-      { COULEUR: color },
-    ],
-    owner: req.user,
-  });
-  const result = await cloudinary.uploader.upload(req.files.picture.path, {
-    folder: "/ecomerce/offers",
-  });
-  newOffer.image = result;
-  await newOffer.save();
-
-  res.json(newOffer);
+    res.json(newOffer);
+  } catch (error) {
+    res.status(400).json({ message: "eroor" });
+  }
 });
 
 module.exports = router;
